1cdea185439bf3009c6b1b3b0aa73b86c00450c5
jacobasuncion <asuncion1173@gmail.com>
2022-05-27 02:28:52 +0800

    Exercise 3.12 commit

---
+++ coding_exer_3.12.py
@@ -0,0 +1,40 @@
+#Part 1 (Just remove comments)
+
+#x=500
+
+#if x > 100:
+#    raise Exception("Error if x is bigger than 100")
+
+#
+
+#Part 2
+
+#try:
+#    print(variable_1)
+#except:
+#    print("The variable does not exist")
+
+#
+
+#Part 3
+#for i in range(6):
+#    print("I'm so happy")
+
+#
+
+#Part 4
+#try:
+#    print(12*6)
+#except:
+#    print("Cannot perform operation")
+#else:
+#    print("This is all good")
+
+#
+
+#Part 5
+best_burger = "Burger King"
+number_2_burger = "McDonald's"
+
+assert best_burger == "Burger Queen"
+assert number_2_burger == "McDonald's"
