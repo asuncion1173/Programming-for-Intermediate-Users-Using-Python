1288aca7f6924734f1655b4afda6ba9abebd6f9e
jacobasuncion <asuncion1173@gmail.com>
2022-05-27 02:52:58 +0800

    Exercise 4.4 commit

---
+++ coding_exer_4.4.py
@@ -0,0 +1,5 @@
+from functionfile import welcome_message
+from functionfile import alert_message
+
+welcome_message()
+alert_message()
---
+++ functionfile.py
@@ -0,0 +1,17 @@
+from tkinter import Y
+
+
+def get_sum(x, y):
+    return x + y
+
+def get_diff(x, y):
+    return x - y
+
+def welcome_message():
+    print("Welcome to the program!")
+
+def alert_message():
+    print("Alert! Take precaution!")
+
+def name_question():
+    print("What's your name?")
